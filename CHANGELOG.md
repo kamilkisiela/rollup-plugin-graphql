9736c497ddbcd6dfa4224cfca695288818535f16
Kamil Kisiela <kamilkisiela@users.noreply.github.com>
2017-06-16 20:56:08 +0100

    Use graphql-tag/loader instead of own implementation (#3)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # rollup-plugin-json changelog
 
+## NEXT
+
+- Use `graphql-tag/loader` directly [PR #3](https://github.com/kamilkisiela/rollup-plugin-graphql/pull/3)
+
 ## 0.0.1
 
 - First release
--- tests/index.spec.js
+++ tests/index.spec.js
@@ -4,14 +4,13 @@ const graphql = require('..');
 process.chdir(__dirname);
 
 describe('plugin', () => {
-  it('should parse a graphql file', () => {
+  it('should parse a simple graphql file', () => {
     return rollup.rollup({
-      entry: 'samples/basic.js',
+      entry: 'samples/basic/index.js',
 			plugins: [graphql()]
 		}).then((bundle) => {
 			const generated = bundle.generate({ format: 'cjs' });
 			const code = generated.code;
-
 			const exports = {};
 			const fn = new Function('exports', code);
 			
@@ -19,6 +18,24 @@ describe('plugin', () => {
 
 			expect(exports.doc).toBeDefined();
 			expect(exports.doc.kind).toBe('Document');
+		});
+  });
+
+	it('should include a fragment', () => {
+    return rollup.rollup({
+      entry: 'samples/fragments/index.js',
+			plugins: [graphql()]
+		}).then((bundle) => {
+			const generated = bundle.generate({ format: 'cjs' });
+			const code = generated.code;
+			const exports = {};
+			const fn = new Function('exports', code);
+			
+			fn(exports);
+
+			expect(exports.doc).toBeDefined();
+			expect(exports.doc.kind).toBe('Document');
+			expect(exports.doc.definitions[1].name.value).toBe('HeroFragment');
 		});
   });
 });
